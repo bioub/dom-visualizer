2515a28dae1144e5037d2c5139adf8d2ecfd0dd0
bioub <romain.bohdanowicz@gmail.com>
2015-02-04 12:04:20 +0100

    New version

---
+++ bak/d3.domVisualizerDelta.js
@@ -0,0 +1,385 @@
+
+
+d3.domVisualizer = function(container, options, undefined) {
+    options || (options = {});
+    this.d3TreeRoot = false,
+        this.duration = options.duration || 500;
+    this.autoIncrement = 0;
+    this.displayTextNodes = options.displayTextNodes || true;
+    this.displayTextIndentNodes = options.displayTextIndentNodes || false;
+    this.displayCommentNodes = options.displayCommentNodes || false;
+    this.overflow = true;
+
+    if (typeof container === 'string') {
+        container = document.querySelector(container);
+    }
+
+    this.tree = d3.layout.tree(); // margin bottom
+    this.diagonal = d3.svg.diagonal()
+        .projection(function (d) {
+            return [d.x, d.y];
+        });
+
+    this.vis = d3.select(container).append("svg:svg")
+
+        .append("svg:g")
+        .attr("transform", "translate(0,40)"); // margin top
+
+    console.log(this.vis);
+
+    this.setSize(container.clientWidth, container.clientHeight);
+}
+
+d3.domVisualizer.prototype.setSize = function(w, h) {
+    this.tree.size([w, h - 65]);
+    d3.select("svg")
+      .attr("width", w)
+      .attr("height", h)
+};
+
+d3.domVisualizer.prototype.autosize = function(pixelPerNode) {
+    var levelWidth = [1];
+
+    var childCount = function (level, n) {
+
+        if (n.children && n.children.length > 0) {
+            if (levelWidth.length <= level + 1) levelWidth.push(0);
+
+            levelWidth[level + 1] += n.children.length;
+            n.children.forEach(function (d) {
+                childCount(level + 1, d);
+            });
+        }
+    };
+    childCount(0, this.d3TreeRoot);
+    var neededWidth = d3.max(levelWidth) * pixelPerNode; // 25 pixels per line
+    var neededHeight = levelWidth.length * 62;
+
+    this.setSize(neededWidth, neededHeight);
+}
+
+d3.domVisualizer.prototype.update = function() {
+
+    var nodes = this.tree.nodes(this.d3TreeRoot);
+
+    nodes.forEach(function(node) {
+       if(!node.children) {
+           node.children = [];
+       }
+    });
+
+    // Select all nodes and syncs with d3TreeRoot
+    var node = this.vis.selectAll("g.node")
+        .data(nodes, function (d) {
+            return d.id;
+        });
+
+    // Move existing nodes to their new position
+    node.transition()
+        .duration(this.duration)
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        })
+        .style("opacity", 1)
+        .select('text')
+            .text(function (d) {
+                return d.value;
+            });
+
+    // Add new nodes
+    var nodeEnter = node.enter().append("svg:g")
+        .attr("class", function (d) {
+            return "node " + d.type;
+        })
+        .style("opacity", 0.5)
+        .attr("transform", function (d) {
+
+            var x = this.d3TreeRoot.x;
+            var y = this.d3TreeRoot.y;
+            var dTmp = d;
+            while(dTmp.par) {
+                if(dTmp.par.x0 && dTmp.par.y0) {
+                    x = dTmp.par.x0;
+                    y = dTmp.par.y0;
+                    break;
+                }
+                dTmp = dTmp.par;
+            }
+
+            return "translate(" + x + "," + y + ")";
+        }.bind(this));
+
+    // Add circle in nodes, radius 10px
+    nodeEnter.append("svg:circle")
+        .attr("r", 10)
+
+    // Add node name 5px above
+    nodeEnter.append("text")
+        .attr("text-anchor", "middle")
+        .attr("dy", -15)
+        .text(function (d) {
+            return d.value;
+        });
+
+    // Move new nodes to their position
+    nodeEnter.transition()
+        .duration(this.duration)
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        })
+        .style("opacity", 1);
+
+    // Remove old nodes
+    node.exit().remove();
+
+    // Select all links
+    var link = this.vis.selectAll("path.link")
+        .data(this.tree.links(nodes), function (d) {
+            return d.target.id;
+        });
+
+    // Insert new links at the parent previous position
+    link.enter().insert("svg:path", "g")
+        .attr("class", "link")
+        .attr("d", function (d) {
+            var x = this.d3TreeRoot.x;
+            var y = this.d3TreeRoot.y;
+            var dTmp = d.source;
+            while(dTmp) {
+                if(dTmp.x0 && dTmp.y0) {
+                    x = dTmp.x0;
+                    y = dTmp.y0;
+                    break;
+                }
+                dTmp = dTmp.par;
+            }
+            x = (d.source && d.source.x0) ? d.source.x0 : x;
+            y = (d.source && d.source.y0) ? d.source.y0 : y;
+            var o = {x: x, y: y};
+            return this.diagonal({source: o, target: o});
+        }.bind(this))
+        .style("opacity", 0);
+
+    // Transition links to their new position.
+    link.transition()
+        .duration(this.duration)
+        .attr("d", this.diagonal)
+        .style("opacity", 1)
+
+    // Remove old links
+    link.exit().remove();
+
+    // Remember current position for future transitions
+    nodes.forEach(function (d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+    });
+
+}
+
+d3.domVisualizer.prototype.getRealNodeName = function (domNode) {
+    var realNodeName = domNode.tagName.toLowerCase();
+
+    if (domNode.hasAttribute("id")) {
+        realNodeName += "#" + domNode.getAttribute("id");
+    }
+    else if (domNode.hasAttribute("class")) {
+        [].forEach.call(domNode.className.split(" "), function (className) {
+            realNodeName += "." + className;
+        });
+    }
+
+    return realNodeName;
+}
+
+d3.domVisualizer.prototype.buildTree = function (domNode, depth) {
+    var d3Node = new DeltaJS.tree.Node();
+    d3Node.id = ++this.autoIncrement;
+    d3Node.depth = depth || 0;
+
+    switch (domNode.nodeType) {
+        case Node.ELEMENT_NODE:
+            d3Node.value = this.getRealNodeName(domNode);
+            d3Node.type = "element";
+
+            if (domNode.childNodes.length) {
+//                d3Node.children = [];
+//                d3Node._children = [];
+                [].forEach.call(domNode.childNodes, function (domChildNode) {
+//                    var d3ChildNode = {};
+                    d3Node.append(this.buildTree(domChildNode, d3Node.depth+1));
+                }.bind(this));
+            }
+
+            break;
+        case Node.TEXT_NODE:
+            d3Node.value = "#text";
+            d3Node.type = domNode.nodeValue.match(/^\s+$/) ? "text indent" : "text";
+
+            break;
+        case Node.COMMENT_NODE:
+            d3Node.value = "#comment";
+            d3Node.type = "comment";
+
+            break;
+    }
+
+    return d3Node;
+}
+
+d3.domVisualizer.prototype.parseHTML = function(htmlStr) {
+    var doc = document.implementation.createHTMLDocument("");
+
+    var domRoot;
+    if (htmlStr.indexOf("<html") !== -1) {
+        doc.documentElement.innerHTML = htmlStr;
+        domRoot = doc.documentElement;
+    }
+    else if (htmlStr.indexOf("<body") !== -1) {
+        doc.documentElement.innerHTML = htmlStr;
+        domRoot = doc.body;
+    }
+    else {
+        doc.body.innerHTML = htmlStr;
+        domRoot = doc.body.firstElementChild;
+    }
+
+    if(this.d3TreeRoot) {
+        var oldTree = this.d3TreeRoot;
+    }
+    //html+head++meta++title+++text+body++h1+++text++div+++h2++++text+++p++++text
+    //html+head++meta++title+++text+body++div+++h2++++text+++p++++text
+    this.d3TreeRoot = this.buildTree(domRoot);
+    if(oldTree) {
+        var removes;
+        var inserts;
+        var updates;
+
+        var xcc;
+
+
+        inserts = [];
+        removes = [];
+        updates = [];
+
+
+        // run xcc
+        matching = new DeltaJS.tree.Matching();
+
+        xcc = new DeltaJS.xcc.Diff(oldTree, this.d3TreeRoot);
+        xcc.matchTrees(matching);
+
+        this.d3TreeRoot.forEach(function(node) {
+           if(!node.partner) {
+               console.log(node);
+           }
+        });
+
+        collector = new DeltaJS.delta.DeltaCollector(matching, oldTree, this.d3TreeRoot);
+        collector.forEachChange(function(op) {
+            switch (op.type) {
+                case DeltaJS.delta.UPDATE_NODE_TYPE:
+                    updates = updates.concat(op.insert);
+                    break;
+
+                case DeltaJS.delta.UPDATE_FOREST_TYPE:
+                    if (op.remove.length > 0) {
+                        removes= removes.concat(op.remove);
+                    }
+
+                    if (op.insert.length > 0) {
+                        inserts = inserts.concat(op.insert);
+                    }
+                    break;
+            }
+        });
+
+        removes.forEach(function(node) {
+            node.par.children.splice(node.childidx, 1);
+            var idx = 0;
+            node.par.children.forEach(function(node) {
+                node.childidx = idx++;
+            })
+        });
+
+        updates.forEach(function(node) {
+            node.partner.value = node.value;
+        });
+
+        inserts.forEach(function(node) {
+            node.par.partner.children.splice(node.childidx, 0, node);
+            node.par = (node.par.partner) ? node.par.partner : node.par;
+            node.forEach(function(n) {
+                n.parent = n.par;
+            });
+            var idx = 0;
+            node.par.partner.children.forEach(function(node) {
+                node.childidx = idx++;
+            });
+        });
+
+        this.d3TreeRoot = oldTree;
+
+        this.d3TreeRoot.forEach(function(node) {
+            if(node.partner) {
+                delete node.partner;
+            }
+        });
+
+//        debugger;
+    }
+//    this.addNodesByType("element");
+//    this.addNodesByType("text");
+//    this.addNodesByType("comment");
+}
+
+function flatten(nodes) {
+    var i, result = [];
+    for (i=0; i < nodes.length; i++) {
+        nodes[i].forEach(function(n) {
+            result.push(n);
+        });
+    }
+    return result;
+}
+
+d3.domVisualizer.prototype.addNodesByType = function (type, node) {
+    node = node || this.d3TreeRoot;
+
+    if(typeof node._children !== "undefined") {
+        node.children = node.children || [];
+
+        var newNodes = node._children.filter(function (elt) {
+            return elt.type === type;
+        });
+
+        var bisect = d3.bisector(function (a, b) {
+            return a.id - b.id;
+        }).right;
+
+        newNodes.forEach(function (elt) {
+            var pos = bisect(node.children, elt);
+            node.children.splice(pos, 0, elt);
+        });
+
+        node.children.forEach(function (childNode) {
+            this.addNodesByType(type, childNode);
+        }.bind(this));
+    }
+};
+
+d3.domVisualizer.prototype.removeNodesByType = function (type, node) {
+    node = node || this.d3TreeRoot;
+
+    if (typeof node._children !== "undefined") {
+
+        for (var pos = 0; pos < node.children.length; pos++) {
+            if (node.children[pos].type.indexOf(type) !== -1) {
+                node.children.splice(pos--, 1);
+            }
+            else {
+                this.removeNodesByType(type, node.children[pos]);
+            }
+        }
+    }
+};
